444310c3d79cae62929aaa8b2c50164be0d5c89b
rdmrdm3 <90216797+rdmrdm3@users.noreply.github.com>
2021-09-14 13:57:13 +0200

    Update help-panda.c

--- help-panda.c
+++ help-panda.c
@@ -6,10 +6,12 @@ int         main()
 {
     char        *example_ptr_to_char_1;
     char        example_ptr_to_char_2[16];
+    char        example_ptr_to_char_3[32] = "this will work too\n";
 
     strcpy(example_ptr_to_char_2, "hello world!\n"); // this will work because pointer is pointing to a memory space where the next 16 block is allocated for usage
     //strcpy(example_ptr_to_char_1, "this will fail because this pointer is pointing to a memory space where the next few memory block is not reserved for usage\n")''
 
     printf("%s", example_ptr_to_char_2);
+    printf("%s", example_ptr_to_char_3);
     return (0);
 }
